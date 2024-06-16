3b29be9c6438f4a50f97157f89d8dd88494648c2
Felipe <fefelbf@gmail.com>
2024-06-15 22:33:12 -0300

    comir

--- script/index.js
+++ script/index.js
@@ -1,10 +1,13 @@
 const inputs = document.getElementsByName('terror');
-const inputAge = document.getElementsByName('age');
-const inputSend = document.querySelector('#second')
+const inputAge = document.getElementById('iage');
+const inputSend = document.querySelector('#second');
 const form = document.querySelector('.form');
 const images = document.querySelector('.div-img');
 
-const sendForm = () =>{
-    form.style.height = '850px';
-    images.style.display = 'block';
+function sendForm(){
+    if(inputAge.value.length === 0){
+        window.alert('[ERROR] select options');
+    } else if(inputs[0].checked){
+        form.style.display = 'none';
+    }
 }
