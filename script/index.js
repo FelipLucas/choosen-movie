1851f73bcdc6f649b75010e6321175e0776846c5
Felipe <fefelbf@gmail.com>
2024-06-15 22:04:00 -0300

    comit

---
+++ script/index.js
@@ -0,0 +1,10 @@
+const inputs = document.getElementsByName('terror');
+const inputAge = document.getElementsByName('age');
+const inputSend = document.querySelector('#second')
+const form = document.querySelector('.form');
+const images = document.querySelector('.div-img');
+
+const sendForm = () =>{
+    form.style.height = '850px';
+    images.style.display = 'block';
+}
